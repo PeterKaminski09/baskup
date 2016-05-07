f0726d3c527780c755a12d227b23aa9091c4a14e
Zach Fogg <zach.fogg@gmail.com>
2016-05-07 07:30:40 +0300

    rewrite and cleanup

--- backUpMessages.sh
+++ backUpMessages.sh
@@ -1,45 +1,40 @@
 #!/bin/sh
 
-#  backUpMessages.sh
-#  
-#
-#  Created by Peter Kaminski on 7/17/15.
-# Main code can be found at https://github.com/kyro38/MiscStuff/blob/master/OSXStuff/.bashrc
-# However I have tweaked it a bit to optimize the result
+
+cd "`dirname "$0"`"
+path_scriptdir="`pwd -P`"
+TMPDIR="$path_scriptdir"'/tmp'
+
 
 #Read all information that our python script dug up
-while IFS='' read -r line || [[ -n $line ]]; do
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    contact="$line"
+    arr_in=(${contact//;/ })
+    contact_number="${arr_in[2]}"
 
-contact=$line
-arrIN=(${contact//;/ })
-contactNumber=${arrIN[2]}
-#Get to the home directory
-cd
-#This path should be the same as the path you use in baskup.sh
-cd ./Downloads/baskup-master
-#Make a directory specifically for this folder
-mkdir $contactNumber
-#Now get into the directory
-cd $contactNumber
-#Perform SQL operations
-sqlite3 ~/Library/Messages/chat.db "
-select is_from_me,text from message where handle_id=(
-select handle_id from chat_handle_join where chat_id=(
-select ROWID from chat where guid='$line')
-)" | sed 's/1\|/Me: /g;s/0\|/Friend: /g' > $line.txt
+    path_contact="$TMPDIR"/"$contact_number"
+    path_attachments="$path_contact"'/attachments'
 
-cd
-cd ./Downloads/baskup-master/$contactNumber
-mkdir "Attachments"
-cd "Attachments"
-#Retrieve the attached stored in the local cache
+    mkdir -p "$path_contact" && cd "$path_contact"
+    sqlite3 ~/Library/Messages/chat.db "
+    select is_from_me,text from message where handle_id=(
+        select handle_id from chat_handle_join where chat_id=(
+            select ROWID from chat where guid='$line'
+        ))" | sed -e 's!^1\(.+\)$!M\1!' \
+                  -e 's!^0\(.+\)$!Y\1!' \
+            | tr '|' '\t' \
+            > "$line".txt \
+        || exit 1
 
-sqlite3 ~/Library/Messages/chat.db "
-select filename from attachment where rowid in (
-select attachment_id from message_attachment_join where message_id in (
-select rowid from message where cache_has_attachments=1 and handle_id=(
-select handle_id from chat_handle_join where chat_id=(
-select ROWID from chat where guid='$line')
-)))" | cut -c 2- | awk -v home=$HOME '{print home $0}' | tr '\n' '\0' | xargs -0 -t -I fname cp fname .
-$line
+    mkdir -p "$path_attachments" && cd "$path_attachments"
+    sqlite3 ~/Library/Messages/chat.db "
+    select filename from attachment where rowid in (
+        select attachment_id from message_attachment_join where message_id in (
+            select rowid from message where cache_has_attachments=1 and handle_id=(
+                select handle_id from chat_handle_join where chat_id=(
+                    select ROWID from chat where guid='$line'
+    ))))" | tr '\n' '\0' | sed -e 's!~/!'"$HOME"/'!g' | xargs -0 -t -I{} cp {} . \
+        || echo "Fail!"
+    #| xargs -0 -t -I{} echo {} \
 done < "$1"
+
